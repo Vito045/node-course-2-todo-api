b69697d7f440c031e378b6e2c91b8646a5685256
Vito045 <vito045@icloud.com>
2018-11-17 14:25:27 +0200

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj)
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.')
+    }
+    console.log('Connected to MongoDB server.');
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log('Todos', count);
+    //     // console.log(JSON.stringify(docs, undefined, 2))
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // })
+
+    db.collection('Users').find({name: 'Andrew'}).toArray().then((arr) => {
+        console.log('Users with name Andrew:');
+        console.log(JSON.stringify(arr, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    })
+
+    db.close();
+});

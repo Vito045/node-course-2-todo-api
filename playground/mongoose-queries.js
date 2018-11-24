c89c70f5c5dec824ce28f4405c9b956d4e8540d9
Vito045 <vito045@icloud.com>
2018-11-24 10:16:53 +0200

    f

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -7,21 +7,21 @@ const {User} = require('./../server/models/user')
 
 var id = '5bf16364ce7efe618652ab3b';
 
-// Todo.find({
-//     _id: id
-// }).then((todos) => {
-//     console.log('Todos', todos)
-// });
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos', todos)
+});
 
-// Todo.findOne({
-//     _id: id
-// }).then((todo) => {
-//     console.log('Todos', JSON.stringify(todo, undefined, 2))
-// });
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todos', JSON.stringify(todo, undefined, 2))
+});
 
-// Todo.findById(id).then((todo) => {
-//     console.log(todo)
-// })
+Todo.findById(id).then((todo) => {
+    console.log(todo)
+})
 
 User.findById(id).then((user) => {
     if(!user){
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 var app = express();
 

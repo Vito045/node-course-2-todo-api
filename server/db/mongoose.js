6899a2cfc272e62d2e3c06df933c59753cba8192
Vito045 <vito045@icloud.com>
2018-11-24 12:44:20 +0200

    uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var MONGODB_URI = 'ds261440.mlab.com:61440/heroku_xd6pt0hz';
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 mongoose.exports = {mongoose}
